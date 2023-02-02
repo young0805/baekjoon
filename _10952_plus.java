700fdec3854d35725cd2a3b707b85012a6a54ab3
James park <53165953+young0805@users.noreply.github.com>
2023-02-02 19:51:27 +0900

    230202_학원에서 푼 알고리즘

---
+++ _10952_plus.java
@@ -0,0 +1,26 @@
+package smaple1;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class _10952_plus {
+ 
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		while(true) {
+		
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		
+		if(A==0 && B ==0) 
+			break; 
+		
+		System.out.println(A+B);
+		}
+	}
+			}
+
+
+
+
---
+++ _3003_king.java
@@ -0,0 +1,25 @@
+package smaple1;
+import java.util.Scanner;
+
+public class _3003_king {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		int C = sc.nextInt();
+		int D = sc.nextInt();			
+		int E = sc.nextInt();	
+		int F = sc.nextInt();			
+
+
+		System.out.println(1-A);
+		System.out.println(1-B);
+		System.out.println(2-C);
+		System.out.println(2-D);
+		System.out.println(2-E);
+		System.out.println(8-F);
+
+	
+		}	
+	}
+
